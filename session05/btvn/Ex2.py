ac7965e5b002f0ae816034453fc30a712dda510c
tranvanthanh99 <thanhvantran99@gmail.com>
2018-08-04 01:57:48 +0700

    03.08.2018

--- session05/btvn/Ex2.py
+++ session05/btvn/Ex2.py
@@ -1,15 +1,11 @@
 number = [1,6,8,1,2,1,5,6]
 print("numbers = ",number)
 
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
 
 count = 0
 for i in number:
     if i == num:
         count+=1
 
-if count >1:
-    print(num,"appers",count,"times in my list")
-else:
-    print(num,"appers",count,"time in my list")
-
+print(num,"appers",count,"time(s) in my list")
